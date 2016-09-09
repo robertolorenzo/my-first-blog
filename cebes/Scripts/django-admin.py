5b70f0dc364312cd96af8589976a422ac0ae6546
rlorenzo <tu@test.test>
2016-09-09 08:52:25 -0500

    mi app djangogirls, mi primer commit

---
+++ cebes/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\inform~1\desktop\myworl~1\django~1\cebes\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ metroid/blog/models.py
@@ -0,0 +1,21 @@
+
+# mi primer modelo para crear entradas de la app blogs
+
+from django.db import models
+from django.utils import timezone
+
+
+class Entradas(models.Model):
+	autor = models.ForeignKey('auth.user')
+	titulo = models.CharField(max_length = 200)
+	texto = models.TextField()
+	fecha_creacion = models.DateTimeField(default = timezone.now)
+	fecha_publicacion = models.DateTimeField(blank = True, null = True)
+
+
+	def publicar(self):
+		self.fecha_publicacion = timezone.now()
+		self.save
+
+	def __STR__(self):
+		return self.titulo
